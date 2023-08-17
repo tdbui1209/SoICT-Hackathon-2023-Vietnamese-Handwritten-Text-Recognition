feacb3dbb939c861f658db53af826cb993720d90
tdbui1209 <tdbui1209@gmail.com>
2023-08-18 01:54:08 +0700

    split data with characters distribution

--- train_val_split.py
+++ train_val_split.py
@@ -1,22 +1,44 @@
 import os
 from utils import Config
-import random
+from collections import Counter
 
 
-ANNOTATION_PATH = "resources/raw_data/BKAI/train_gt.txt"
+ANNOTATION_PATH = "resources/raw_data/train_gt.txt"
 
+TRAIN_ANNOTATIONS = open(os.path.join(Config.DATA_DIR, "train_gt.txt"), "w", encoding="utf-8")
+TEST_ANNOTATIONS = open(os.path.join(Config.DATA_DIR, "test_gt.txt"), "w", encoding="utf-8")
 
-TRAIN_ANNOTATIONS = open(os.path.join(Config.REC_DATA_DIR, "train_gt.txt"), "w", encoding="utf-8")
-TEST_ANNOTATIONS = open(os.path.join(Config.REC_DATA_DIR, "test_gt.txt"), "w", encoding="utf-8")
+image_path_list = []
+word_list = []
+with open(ANNOTATION_PATH, "r", encoding="utf-8") as f:
+    for line in f:
+        image_path, word = line.strip().split(None, 1)
+        image_path_list.append(image_path)
+        word_list.append(word)
 
-ANNOTATIONS = open(ANNOTATION_PATH, "r", encoding="utf-8")
-ANNOTATIONS = ANNOTATIONS.readlines()
-random.shuffle(ANNOTATIONS)
+train_set = []
+test_set = []
 
+train_letter_counts = Counter()
+test_letter_counts = Counter()
 
-ratio = 0.8
-for line in ANNOTATIONS[:int(len(ANNOTATIONS) * ratio)]:
-    TRAIN_ANNOTATIONS.write(line)
+for imaeg_path, word in zip(image_path_list, word_list):
+    c = Counter(word)
+    should_add_to_test = True
+    for letter, count in c.items():
+        if train_letter_counts[letter] <= test_letter_counts[letter] * 8:
+            should_add_to_test = False
+            break
 
-for line in ANNOTATIONS[int(len(ANNOTATIONS) * ratio):]:
-    TEST_ANNOTATIONS.write(line)
+    if should_add_to_test:
+        test_set.append((imaeg_path, word))
+        test_letter_counts.update(c)
+    else:
+        train_set.append((imaeg_path, word))
+        train_letter_counts.update(c)
+
+for image_path, word in train_set:
+    TRAIN_ANNOTATIONS.write(image_path + "\t" + word + "\n")
+
+for image_path, word in test_set:
+    TEST_ANNOTATIONS.write(image_path + "\t" + word + "\n")
